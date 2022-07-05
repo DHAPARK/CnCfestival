1e6c8d21242753d8ff34c5d4ae6c712d925467a9
DHAPARK <pdh4182@gmail.com>
2022-07-06 05:51:36 +0900

    파이썬웹컴파일

---
+++ monacoWebPythonCompile/router.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const fs = require('fs');
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
+
+//임시 파일명 : 숫자 pf + cnt 숫자 + .py
+var cnt = 0;
+
+//알파벳,숫자인지 체크하는 메서드
+function isAlphaNumCheck(str) {
+    const rgxstr = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j"
+,"k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J"
+,"K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",":","(",")","_"," ","\n","=",'"',"'","+",
+"-","*","/","!","#","@","$","%","^","&","\\","/","|",".",",","?","<",">","`","~","[","]","\t","/t"]
+    var Flag = false;
+    for(var i=0;i<rgxstr.length;i++){
+        if(str == rgxstr[i]){
+            Flag = true;
+        }
+    }
+
+    return Flag;
+}
+
+app.post('/makeAndCompilePythonFile',async (req,res)=>{
+    //파일을 만들어야함 먼저
+    //1. 코드가 제대로 넘어오는지를 확인
+    
+    //코드가 제대로 넘어오니 넘어오는코드로 .py 파일을 새로만들어야함
+    var code = req.body.code;
+    
+    //내가볼때 문자열을 하나하나 탐색해서 싹다 " "공백으로 바꿔놔야한다
+    //진짜 죽겠네 ;;하..
+    
+    for(var i=0; i < code.length ; i++){
+        if(!isAlphaNumCheck(code[i])){
+            code = code.replace(code[i]," ");
+        }
+    }
+
+    fs.writeFileSync(`pf${cnt++}.py`,code,"utf8",(err)=>{
+        if(err){
+            console.log(`${err}\npython 파일생성에 문제발생`);
+        }
+    })
+    //파일제대로 생기나 확인해야함
+    
+    //파일이 제대로 생성이 되는걸 확인했으니 "방금 만들어진" 파이썬파일 그대로 컴파일
+    const spawn = require("child_process").spawn;
+    const result = spawn('python',[`pf${cnt-1}.py`]);
+    
+    //성공시 이렇게 결과받는다.
+    result.stdout.on('data',(data)=>{
+        console.log(data.toString());
+    })
+    //에러발생시 이렇게 받고 아래는 코드 출처 ㅇㅇ
+    result.stderr.on('data', (data)=>{
+        console.log(data.toString());
+    });
+    //출처: https://curryyou.tistory.com/225 [카레유:티스토리]    
+
+    res.send("갔나");
+})
+
+app.listen(3000,()=>{
+    console.log('3000번 포트로 포트 실행중..');
+})
