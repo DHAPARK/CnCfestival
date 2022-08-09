e82cbe4fef173bf5db0074ad1fe12a503a46862c
ParkJinYong <mej1406@gmail.com>
2022-08-09 18:34:32 +0900

    웹 서버 테스트1

--- node_server/router/web_route/web_test.js
+++ node_server/router/web_route/web_test.js
@@ -11,7 +11,7 @@ router.get('/index',(req,res)=>{
 router.get('/regist',(req,res)=>{
     userAgentModel.printUserAgent(req.header('user-agent'),"/regist");
     
-    res.render('regist');
+    res.render('regist1');
 })
 
 /**
