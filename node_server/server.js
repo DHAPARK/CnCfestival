5663fb79dfcda809d43b64d6e4048a2fca56326b
ParkJinYong <mej1406@gmail.com>
2022-08-09 17:29:58 +0900

    웹 서버 테스트1

--- node_server/server.js
+++ node_server/server.js
@@ -34,6 +34,8 @@ app.use(bodyParser.json());
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.static('img'));
 
 const appUtilRouter = require('./router/app_route/util');
 const appUserRouter = require('./router/app_route/user');
