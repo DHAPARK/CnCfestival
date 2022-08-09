4440be0d971e42380f927ebcffda82ec3429d19f
ParkJinYong <mej1406@gmail.com>
2022-08-09 17:13:47 +0900

    웹 서버 테스트1

--- node_server/router/web_route/web_test.js
+++ node_server/router/web_route/web_test.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const userAgentModel = require('../../models/userAgentModel');
 
+router.get('/',(req,res)=>{
+    userAgentModel.printUserAgent(req.header('user-agent'),"/");
+
+    res.render('index');
+})
+
+router.get('/regist',(req,res)=>{
+    userAgentModel.printUserAgent(req.header('user-agent'),"/regist");
+    
+    res.render('regist');
+})
+
 /**
  * 회원가입
  * @method post
