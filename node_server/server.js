a4eabe87ebd502c087b34fa07550fddd249e66e3
ParkJinYong <mej1406@gmail.com>
2022-08-11 14:04:05 +0900

    웹 서버 테스트2

--- node_server/server.js
+++ node_server/server.js
@@ -27,7 +27,7 @@ const envConfig = require('./config/envConfig');
 
 app.use(logger('dev'));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 // app.use(express.json());
 // app.use(express.urlencoded({extended:false}));
