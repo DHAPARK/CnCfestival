b02693fc9c6662c304aad51463ce0011e9caeada
ParkJinYong <mej1406@gmail.com>
2022-08-19 22:36:50 +0900

    웹 연결 테스트

--- node_server/server.js
+++ node_server/server.js
@@ -32,7 +32,8 @@ app.use(cookieParser());
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/resource'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/img'));
 
 const appUtilRouter = require('./router/app_route/util');
 const appUserRouter = require('./router/app_route/user');
