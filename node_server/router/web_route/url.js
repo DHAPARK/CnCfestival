f14b4407b9a06757cb783a09dc2d165539d4a49d
vanny0421 <vannykim21@hotmail.com>
2022-08-28 01:50:45 +0900

    0828

--- node_server/router/web_route/url.js
+++ node_server/router/web_route/url.js
@@ -160,7 +160,8 @@ router.get("/videoChild", async (req, res) => {
     watchTime: videoLog.watchTime,
     watchComplete: videoLog.watchComplete,
   };
-
+  res.setHeader("Access-Control-Allow-Origin: http://localhost:80");
+  res.setHeader("Access-Control-Allow-Origin: https://www.youtube.com");
   res.render("videoChild", { datas: datas });
 });
 
