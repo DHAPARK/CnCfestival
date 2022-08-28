df66ef9930dfafe467a66ca943919aec33328c5e
장정윤 <jeinie9179@naver.com>
2022-08-28 13:53:47 +0900

    0828

--- node_server/router/web_route/url.js
+++ node_server/router/web_route/url.js
@@ -81,8 +81,9 @@ router.get("/market/:page", async (req, res) => {
     console.log(`_marketInfo ${_marketInfo}`);
     res.render("market", { marketInfo: _marketInfo, totalPage: totalPage });
   } else {
-    res.redirect('http://220.67.231.91:80/web/market');
-    //location.href = "/market";
+    //res.redirect('http://220.67.231.91:80/web/market');
+    
+    location.href = "http://220.67.231.91:80/web/market";
   }
   
 });
