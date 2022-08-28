36b0ac48c898faf9763c508f3ea0469f7d36df93
ParkJinYong <mej1406@gmail.com>
2022-08-28 16:29:36 +0900

    0828

--- node_server/server.js
+++ node_server/server.js
@@ -113,33 +113,36 @@ app.post("/test", async (req, res) => {
   //console.log(`quizNum ${quizNum} ${typeof(quizNum)}`);
 
   //fs.writeFileSync(`/submit/${userId}_${quizNum}.py`, code, "utf8", (err) => {
-  fs.writeFileSync(`/submit/${userId}_${1}.py`, code, "utf8", (err) => {
+  fs.writeFile(`/submit/${userId}_${1}.py`, code, "utf8", (err, data) => {
     if (err) {
       console.log(`${err}\npython 파일생성에 문제발생`);
+    }
+    if (data) {
+      //fs.readFileSync(`cat /answer/input_answer${quizNum}.txt`, "utf-8", (err, data) => {
+      fs.readFileSync(`cat /answer/input_answer${1}.txt`, "utf-8", (err, data) => {
+        if (err) {
+          console.log(`${err}\n파일 로딩에 문제발생`);
+        }
+        console.log(`###answer data = ${data}`)
+
+        // await exec(`cat input${quizNum} | python3 /submit/pf${0}.py`, { shell: true }, (error, stdout) => {
+        //   if (error) {
+        //     console.log(`stdout ${stdout}`);
+        //     console.log(`error ${error}`);
+        //     res.json({ code: 100, stdout: stdout, stderr: error.message });
+        //   } else {
+        //     console.log(`stdout ${stdout}`);
+        //     console.log(`error ${error}`);
+        //     res.json({ code: 200, stdout: stdout, stderr: error });
+        //   }
+        // });
+      }); 
     }
   });
   //파일제대로 생기나 확인해야함
 
   //파일이 제대로 생성이 되는걸 확인했으니 "방금 만들어진" 파이썬파일 그대로 컴파일
-  //fs.readFileSync(`cat /answer/input_answer${quizNum}.txt`, "utf-8", (err, data) => {
-  fs.readFileSync(`cat /answer/input_answer${1}.txt`, "utf-8", (err, data) => {
-    if (err) {
-      console.log(`${err}\n파일 로딩에 문제발생`);
-    }
-    console.log(`###answer data = ${data}`)
-
-    // await exec(`cat input${quizNum} | python3 /submit/pf${0}.py`, { shell: true }, (error, stdout) => {
-    //   if (error) {
-    //     console.log(`stdout ${stdout}`);
-    //     console.log(`error ${error}`);
-    //     res.json({ code: 100, stdout: stdout, stderr: error.message });
-    //   } else {
-    //     console.log(`stdout ${stdout}`);
-    //     console.log(`error ${error}`);
-    //     res.json({ code: 200, stdout: stdout, stderr: error });
-    //   }
-    // });
-  }); 
+  
 });
 
 
