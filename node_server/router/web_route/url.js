612d9acaebd4dd51d5d8f3794b6b7be71ad74ccd
ParkJinYong <mej1406@gmail.com>
2022-08-28 15:33:20 +0900

    0828

--- node_server/router/web_route/url.js
+++ node_server/router/web_route/url.js
@@ -149,9 +149,9 @@ router.get("/mypage", async (req, res) => {
 
 router.get("/solution", async (req, res) => {
   userAgentModel.printUserAgent(req.header("user-agent"), "/solution");
-  let quizNum = req.params.quizNum;
-  let userId = req.params.userId;
-  
+  let quizNum = req.query.quizNum;
+  let userId = req.query.userId;
+
   let quizInfo = await getQuizInfo();
   console.log(`userId ${userId}`);
   console.log(`quizNum ${quizNum}`);
