175be01845c0ee5632e5d584806d4916bb2975f8
CatJelly <70837543+CatJelly@users.noreply.github.com>
2022-09-04 21:00:04 +0900

    0904-1806 update

--- node_server/router/web_route/util.js
+++ node_server/router/web_route/util.js
@@ -172,6 +172,8 @@ router.post("/saveVideoLog", async (req, res) => {
   console.log(`watchComplete : ${watchComplete}`);
   console.log(`videoTotalLength : ${videoTotalLength}`);
 
+  watchTime = int(watchTime);
+  videoTotalLength = int(videoTotalLength);
   watchComplete = watchTime == videoTotalLength - 1 ? true : false;
   let ps = await global.db.collection(DB_COLLECTION["VIDEO_LOG"]);
   ps.doc(userId)
