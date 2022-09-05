6c2fa857cffe1516e70e1324c09b7e0d58138347
DHAPARK <pdh4182@gmail.com>
2022-09-05 22:25:41 +0900

    0905

--- node_server/router/web_route/transaction.js
+++ node_server/router/web_route/transaction.js
@@ -1,12 +1,11 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userAgentModel = require('../../models/userAgentModel');
+const userAgentModel = require("../../models/userAgentModel");
 const { suppressDeprecationWarnings } = require("moment");
 
-const { RETURN_CODE } = require('../../utils/constant');
-const { remittanceCoin, paymentCoin } = require('../../utils/transaction');
-const { isPasswordRight } = require('../../utils/validation');
+const { RETURN_CODE } = require("../../utils/constant");
+const { remittanceCoin, paymentCoin } = require("../../utils/transaction");
+const { isPasswordRight } = require("../../utils/validation");
 const SUCCESS_CODE = "SUCCESS_CODE";
 
 /**
@@ -17,34 +16,38 @@ const SUCCESS_CODE = "SUCCESS_CODE";
  * @param {string} senderAddress 전송측 주소
  * @param {string} receiverAddress 송신측 주소
  * @param {int} amount 보낼 금액
- * @returns {object} 성공 코드 
+ * @returns {object} 성공 코드
  * (성공: 100, 송신측 주소 없음: 200, 잔액부족: 201, 비밀번호 오류: 202)
  */
-router.post('/hscRemittance', async (req, res)=> {
-    userAgentModel.printUserAgent(req.header('user-agent'),"/hscRemittance");
-    
-    const userId = req.body["userId"];
-    const userPassword = req.body["userPassword"];
-    const senderAddress = req.body["senderAddress"];
-    const receiverAddress = req.body["receiverAddress"];
-    const amount = req.body["amount"];
-    const result = {};
+router.post("/hscRemittance", async (req, res) => {
+  userAgentModel.printUserAgent(req.header("user-agent"), "/hscRemittance");
 
-    console.log(`### /hscRemittance : data`);
-    console.log(`userId = ${userId}`);
-    console.log(`userPassword = ${userPassword}`);
-    console.log(`senderAddress = ${senderAddress}`);
-    console.log(`receiverAddress = ${receiverAddress}`);
-    console.log(`amount = ${amount}`);
-    
-    if(await isPasswordRight(userId, userPassword)) {
-        result[SUCCESS_CODE] = await remittanceCoin(senderAddress, receiverAddress, amount);
-    } else {
-        result[SUCCESS_CODE] = RETURN_CODE['PASSWORD_ERR'];
-    }
-    console.log(`result = ${result}`);
-    res.json(result);
-})
+  const userId = req.body["userId"];
+  const userPassword = req.body["userPassword"];
+  const senderAddress = req.body["senderAddress"];
+  const receiverAddress = req.body["receiverAddress"];
+  const amount = req.body["amount"];
+  const result = {};
+
+  console.log(`### /hscRemittance : data`);
+  console.log(`userId = ${userId}`);
+  console.log(`userPassword = ${userPassword}`);
+  console.log(`senderAddress = ${senderAddress}`);
+  console.log(`receiverAddress = ${receiverAddress}`);
+  console.log(`amount = ${amount}`);
+
+  if (await isPasswordRight(userId, userPassword)) {
+    result[SUCCESS_CODE] = await remittanceCoin(
+      senderAddress,
+      receiverAddress,
+      amount
+    );
+  } else {
+    result[SUCCESS_CODE] = RETURN_CODE["PASSWORD_ERR"];
+  }
+  console.log(`result = ${result}`);
+  res.json(result);
+});
 
 /**
  * 결제
@@ -54,40 +57,40 @@ router.post('/hscRemittance', async (req, res)=> {
  * @param {string} senderAddress 전송측 주소
  * @param {string} receiverAddress 송신측 주소
  * @param {int} amount 보낼 금액
- * @returns {object} 성공 코드 
+ * @returns {object} 성공 코드
  * (성공: 100, 송신측 주소 없음: 200, 잔액부족: 201, 비밀번호 오류: 202)
  */
-router.post('/hscPayment', (req, res)=> {
-    userAgentModel.printUserAgent(req.header('user-agent'),"/hscPayment");
+router.post("/hscPayment", (req, res) => {
+  userAgentModel.printUserAgent(req.header("user-agent"), "/hscPayment");
 
-    const userId = req.body["userId"];
-    const userPassword = req.body["userPassword"];
-    console.log(typeof(userPassword), userPassword);
-    const senderAddress = req.body["senderAddress"];
-    const receiverAddress = req.body["receiverAddress"];
-    const amount = req.body["amount"];
-    const result = {};
+  const userId = req.body["userId"];
+  const userPassword = req.body["userPassword"];
+  console.log(typeof userPassword, userPassword);
+  const senderAddress = req.body["senderAddress"];
+  const receiverAddress = req.body["receiverAddress"];
+  const amount = req.body["amount"];
+  const result = {};
 
-    console.log(`### /hscPayment : data`);
-    console.log(`userId = ${userId}`);
-    console.log(`userPassword = ${userPassword}`);
-    console.log(`senderAddress = ${senderAddress}`);
-    console.log(`receiverAddress = ${receiverAddress}`);
-    console.log(`amount = ${amount}`);
+  console.log(`### /hscPayment : data`);
+  console.log(`userId = ${userId}`);
+  console.log(`userPassword = ${userPassword}`);
+  console.log(`senderAddress = ${senderAddress}`);
+  console.log(`receiverAddress = ${receiverAddress}`);
+  console.log(`amount = ${amount}`);
 
-    isPasswordRight(userId, userPassword).then(isSucc => {
-        if(isSucc == true) {
-            paymentCoin(senderAddress, receiverAddress, amount).then(code => {
-                result['SUCCESS_CODE'] = code;
-                console.log(`result = ${result}`);
-                res.json(result);
-            })              
-        } else {
-            result[SUCCESS_CODE] = RETURN_CODE['PASSWORD_ERR'];
-            console.log(`result = ${result}`);
-            res.json(result);
-        }
-    });
-})
+  isPasswordRight(userId, userPassword).then((isSucc) => {
+    if (isSucc == true) {
+      paymentCoin(senderAddress, receiverAddress, amount).then((code) => {
+        result["SUCCESS_CODE"] = code;
+        console.log(`result = ${result}`);
+        res.json(result);
+      });
+    } else {
+      result[SUCCESS_CODE] = RETURN_CODE["PASSWORD_ERR"];
+      console.log(`result = ${result}`);
+      res.json(result);
+    }
+  });
+});
 
 module.exports = router;
