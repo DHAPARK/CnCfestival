eb5a5c66bedde5594e6e110ee628ca12ec225684
DHPARK <37052379+DHAPARK@users.noreply.github.com>
2022-09-16 01:17:34 +0900

    0916

--- node_server/config/envConfig.js
+++ node_server/config/envConfig.js
@@ -1,22 +1,22 @@
 const admin = require("firebase-admin");
 const firestore = require("firebase-admin/firestore");
 const serviceAccount = require("../hscoin-d8ff7-firebase-adminsdk-unmpe-a6a77a60b5.json");
-const Web3 = require('web3');
+const Web3 = require("web3");
 
-const { HSCOIN_ABI, HSCOIN_ADDRESS } = require('../utils/constant');
+const { HSCOIN_ABI, HSCOIN_ADDRESS } = require("../utils/constant");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAHUg25ak_qTeKbHathmfnMuey4UeJTrkQ",
-    authDomain: "hscoin-d8ff7.firebaseapp.com",
-    projectId: "hscoin-d8ff7",
-    storageBucket: "hscoin-d8ff7.appspot.com",
-    messagingSenderId: "928676142936",
-    appId: "1:928676142936:web:72e7970feb2b29c792cf2d",  
-    measurementId: "G-86FLCDRTND",
-    credential: admin.credential.cert(serviceAccount)
+  apiKey: "AIzaSyAHUg25ak_qTeKbHathmfnMuey4UeJTrkQ",
+  authDomain: "hscoin-d8ff7.firebaseapp.com",
+  projectId: "hscoin-d8ff7",
+  storageBucket: "hscoin-d8ff7.appspot.com",
+  messagingSenderId: "928676142936",
+  appId: "1:928676142936:web:72e7970feb2b29c792cf2d",
+  measurementId: "G-86FLCDRTND",
+  credential: admin.credential.cert(serviceAccount),
 };
 
-const { Storage } = require('@google-cloud/storage');
+const { Storage } = require("@google-cloud/storage");
 // const { credential } = require("firebase-admin");
 // const projectId = 'hscoin-d8ff7';
 // const storage = new Storage({
@@ -25,33 +25,35 @@ const { Storage } = require('@google-cloud/storage');
 // });
 
 async function generateV4ReadSignedUrl() {
-    // These options will allow temporary read access to the file
-    const options = {
-        version: 'v4',
-        action: 'read',
-      expires: Date.now() + 15 * 60 * 1000, // 15 minutes
-    };
+  // These options will allow temporary read access to the file
+  const options = {
+    version: "v4",
+    action: "read",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  };
 
-    // Get a v4 signed URL for reading the file
-    const [url] = await storage
-        .bucket('test')
-        .file('test_img1.jpeg')
-        .getSignedUrl(options);
+  // Get a v4 signed URL for reading the file
+  const [url] = await storage
+    .bucket("test")
+    .file("test_img1.jpeg")
+    .getSignedUrl(options);
 
-    console.log('Generated GET signed URL:');
-    console.log(url);
-    console.log('You can use this URL with any user agent, for example:');
-    console.log(`curl '${url}'`);
+  console.log("Generated GET signed URL:");
+  console.log(url);
+  console.log("You can use this URL with any user agent, for example:");
+  console.log(`curl '${url}'`);
 }
 
 /**
  * Web3, HsContract 객체 생성
  */
 async function initWeb3() {
-    global.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-    global.accountList = await web3.eth.getAccounts();
-    global.hsContract = new web3.eth.Contract(HSCOIN_ABI, HSCOIN_ADDRESS);
-    console.log(`### Web3 Init`);
+  global.web3 = new Web3(
+    new Web3.providers.HttpProvider("http://localhost:8545")
+  );
+  global.accountList = await web3.eth.getAccounts();
+  global.hsContract = new web3.eth.Contract(HSCOIN_ABI, HSCOIN_ADDRESS);
+  console.log(`### Web3 Init`);
 }
 
 global.firebaseAdmin = admin.initializeApp(firebaseConfig);
@@ -62,14 +64,13 @@ global.firebaseAdmin = admin.initializeApp(firebaseConfig);
 // });
 
 async function initDB() {
-    global.db = firestore.getFirestore();
-    global.storage = await global.firebaseAdmin.storage();
+  global.db = firestore.getFirestore();
+  global.storage = await global.firebaseAdmin.storage();
 }
-
 
 module.exports = {
-    firestore,
-    initWeb3,
-    initDB,
-    generateV4ReadSignedUrl
-}
+  firestore,
+  initWeb3,
+  initDB,
+  generateV4ReadSignedUrl,
+};
