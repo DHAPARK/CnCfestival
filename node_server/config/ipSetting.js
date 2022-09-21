0ff9b874e4e1d7bd4f632c6880ed5366d24dca38
CatJelly <70837543+CatJelly@users.noreply.github.com>
2022-09-21 23:37:44 +0900

    마무리 ip update

--- node_server/config/ipSetting.js
+++ node_server/config/ipSetting.js
@@ -1,6 +1,6 @@
 //const ipConfig = "http://13.113.58.8";
 //const ipConfig = "http://35.78.201.84"; // 도현
-const ipConfig = "http://220.67.231.91"; // 도현
+const ipConfig = "http://52.79.236.250"; // 진용
 
 module.exports = {
   ipConfig,
